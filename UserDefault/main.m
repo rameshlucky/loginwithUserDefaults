4550b6219d3e74d095fa4963e95708315f7b85db
fis <fis@fiss-mac-2.local>
2016-05-13 02:05:33 -0700

    Initial Commit

---
+++ UserDefault/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UserDefault
+//
+//  Created by fis on 13/05/16.
+//  Copyright © 2016 fis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
